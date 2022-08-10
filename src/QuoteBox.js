d6558e907fd0a85713f1e37e9cf6e96fb7a0bf9e
markoter <markoterdev@outlook.com>
2022-08-10 18:27:10 +0200

    testing pages update

--- src/QuoteBox.js
+++ src/QuoteBox.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const QuoteBox = () => {
     return(<div id="quote-box">
-    and this is quote box
+    and this is quote box and now im testing if deploy to pages works
     <div id="text">text</div>
     <div id="author">author</div>
     <button id="new-quote">New Quote</button>
