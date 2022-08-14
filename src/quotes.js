a3793bfe4ac98c6778728ad17d51bb3f5ad1ac38
markoter <markoterdev@outlook.com>
2022-08-14 06:16:38 +0200

    make list of some filler quotes

---
+++ src/quotes.js
@@ -0,0 +1,53 @@
+const quotesList = [
+    {
+        index: 1,
+        author: "Oscar Wilde",
+        quote: "Be yourself; everyone else is already taken."
+    },
+    
+    {
+        index: 2,
+        author: "Albert Einstein",
+        quote: "Two things are infinite: the universe and human stupidity; and I'm not sure about the universe."
+    },
+    {
+        index: 3,
+        author: "Walt Disney",
+        quote: "The way to get started is to quit talking and begin doing"
+    },
+    {
+        index: 4,
+        author: "James Cameron",
+        quote: "If you set your goals ridiculously high and it's a failure, you will fail above everyone else's success."
+    },
+    {
+        index: 5,
+        author: "Benjamin Franklin",
+        quote: "Tell me and I forget. Teach me and I remember. Involve me and I learn."
+    },
+    {
+        index: 6,
+        author: "Cardinal Ximénez",
+        quote: "Nobody expects the Spanish Inquisition!"
+    },
+    {
+        index: 7,
+        author: "Ray Bradbury",
+        quote: "Life is trying things to see if they work."
+    },
+    {
+        index: 8,
+        author: "Winston Churchill",
+        quote: "Success is not final; failure is not fatal: It is the courage to continue that counts."
+    },
+    {
+        index: 9,
+        author: "Barack Obama",
+        quote: "The real test is not whether you avoid this failure, because you won't. It's whether you let it harden or shame you into inaction, or whether you learn from it; whether you choose to persevere."
+    },
+    {
+        index: 10,
+        author: "Ralph Waldo Emerson",
+        quote: "The only person you are destined to become is the person you decide to be."
+    },
+]
