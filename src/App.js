50f53e1902d7eda92ed6a5d2ba7c8b63249fadc5
markoter <markoterdev@outlook.com>
2022-08-14 08:35:57 +0200

    connect quote chooser with state

--- src/App.js
+++ src/App.js
@@ -2,10 +2,19 @@ import React from "react";
 import QuoteBox from "./QuoteBox";
 
 class App extends React.Component {
+    state = {
+        quoteNumberRandom: 0
+    }
+    selectRandomQuote = () => {
+        const quotesAmmount = 10 //TODO this should be length of quotesList
+        const rndNum = Math.floor(Math.random() * quotesAmmount)
+        this.setState({quoteNumberRandom: rndNum})
+    } 
     render() {
+        const {quoteNumberRandom} = this.state
         return (<div id="app-container">
-            <p>this is app</p>
-            <QuoteBox />
+            <p>this is app, numberTest: {quoteNumberRandom}</p>
+            <QuoteBox quoteNumber={quoteNumberRandom}/>
         </div>
         )
     }
--- src/QuoteBox.js
+++ src/QuoteBox.js
@@ -1,12 +1,13 @@
 import React from "react";
 import quotesList from "./quotes";
 
-const QuoteBox = () => {
-    return(<div id="quote-box">
-    <div id="text">{quotesList[3].quote}</div>
-    <div id="author">{quotesList[3].author}</div>
-    <button id="new-quote">New Quote</button>
-    <a href="https://www.twitter.com" target="_blank" id="tweet-quote">Tweet</a>
+const QuoteBox = (props) => {
+    const { quoteNumber } = props
+    return (<div id="quote-box">
+        <div id="text">{quotesList[quoteNumber].quote}</div>
+        <div id="author">{quotesList[quoteNumber].author}</div>
+        <button id="new-quote">New Quote</button>
+        <a href="https://www.twitter.com" target="_blank" id="tweet-quote">Tweet</a>
     </div>)
 }
 
