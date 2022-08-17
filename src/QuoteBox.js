8daf06a37cecb3316796f47c2eb4be2decd0cee9
markoter <markoterdev@outlook.com>
2022-08-17 22:04:23 +0200

    workiing on tweet button

--- src/QuoteBox.js
+++ src/QuoteBox.js
@@ -3,12 +3,15 @@ import quotesList from "./quotes";
 
 const QuoteBox = (props) => {
     const { quoteNumber } = props
+    const quoteText = quotesList[quoteNumber].quote
+    const quoteAuthor = quotesList[quoteNumber].author
+    const encodedQuoteText = encodeURIComponent(quoteText)
     return (
         <div id="quote-box">
-            <div id="text">{quotesList[quoteNumber].quote}</div>
-            <div id="author">{quotesList[quoteNumber].author}</div>
+            <div id="text">{quoteText}</div>
+            <div id="author">{quoteAuthor}</div>
             <button id="new-quote" onClick={() => props.selectRandomQuote()}>New Quote</button>
-            <a href="https://www.twitter.com" target="_blank" id="tweet-quote">Tweet</a>
+            <a href={`https://twitter.com/intent/tweet?text=${encodeURIComponent(quoteText)}`} target="_blank" id="tweet-quote">Tweet</a>
         </div>
     )
 }
